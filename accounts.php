6a89347f88274595df5bc9f2f09aa898ca4bb0ec
Shadab Ahmed <shadaba@kth.se>
2021-04-02 02:11:42 +0200

    added account page

--- accounts.php
+++ accounts.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Accounts</title>
 
     <!-- bootstrap file via jsDelivr -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
@@ -59,12 +59,12 @@
 
         <!-- ==================== Start Banner Area ==================== -->
 
-        <section id="scroll" class="site-banner-admin">
-            <div class="bg-image-admin"></div>
+        <section id="scroll" class="site-banner-accounts">
+            <div class="bg-image-accounts"></div>
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-7">
-                            <h3 class="admin-title text-center">Admin</h3>
+                            <h3 class="accounts-title text-center">Accounts</h3>
                             <h2></h2>
                         </div>    
                     </div> 
@@ -74,24 +74,7 @@
                 <div class="wrapper">
                     <div class="dashboard-title">
                         <h1 class="title text-center">Dashboard</h1>
-                    </div>
-                    <div class="col-3">
-                        <div class="box text-center ">
-                            <h1>5</h1>
-                            <br />
-                            Categories
-                        </div>
-                        <div class="box text-center ">
-                            <h1>5</h1>
-                            <br />
-                            Categories
-                        </div>
-                        <div class="box text-center ">
-                            <h1>5</h1>
-                            <br />
-                            Categories
-                        </div>
-                    </div>    
+                    </div>  
                     <div class="clearfix"></div>
                 </div>
             </div>
