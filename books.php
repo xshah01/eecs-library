2b860b7029d30ea9e17850d9e6590e9bcf544963
xshah01 <shadab.ahmed@live.se>
2021-04-13 17:56:15 +0200

    update book page

--- books.php
+++ books.php
@@ -67,115 +67,11 @@
                             <h3 class="services-title text-center">Books</h3>
                             <h2></h2>
                         </div>
-
-                        <div class="row">
-                            <div class="column-left">
-                                <div class="services-title">
-                                    <div class="col-lg-7">
-                                        <h1>Our books</h1>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="column-right"></div>
-                        </div>
-
-                        <div class="row">
-                                <div class="column-left">
-                                    <div class="ask-a-librarian">
-                                        <div class="col-xl-11">
-                                        <h4>Ask a librarian</h4>
-                                        <p>Have a question? Ask us now! Librarians are available to answer your questions and help with your research. 
-                                            We are happy to help you, but asking for your patience. As many of us are working remotely, this may impact our 
-                                            access to some EECS library materials and our ability to fully respond to your question at this time. </p>
-                                            <a href="contact.html"><p class="link-to-contact">Use our conctact form to get in touch with a librarian › </p></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            <div class="column-right">                              
-                                <div class="services-video1"> 
-                                    <video width="500" height="375" autoplay loop>
-                                        <source src="../eecs-library/img/services-video1.mp4" type="video/mp4">
-                                        <source src="../eecs-library/img/services-video1.mp4" type="video/ogg">
-                                        Your browser does not support the video tag.
-                                      </video>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="row">
-                            <div class="column-left">
-                                <div class="study-booth">
-                                    <div class="col-xl-11">
-                                    <h4>Study booth</h4>
-                                    <p>There are several reading and study booths available for quiet sessions, 
-                                        if you prefer to study in a quiet environment.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        <div class="column-right">                              
-                            <div class="services-video2"> 
-                                <video width="500" height="375" autoplay loop>
-                                    <source src="../eecs-library/img/services-video2.mp4" type="video/mp4">
-                                    <source src="../eecs-library/img/services-video2.mp4" type="video/ogg">
-                                    Your browser does not support the video tag.
-                                  </video>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="column-left">
-                            <div class="recycle">
-                                <div class="col-xl-11">
-                                <h4>Book recycle</h4>
-                                <p>Books are an important part of our lives but many of us still struggle with old and 
-                                    unwanted books that takes up space in our shelves. When you decide it's time to part with them, 
-                                    we want you to know that they can go to a nice home where they can continue to enrich and improve 
-                                    other student's education. We gracefully accept all kinds of books.</p>
-                                    <a href="contact.html"><p class="link-to-contact">Find us and start recycling your books › </p></a>
-                                </div>
-                            </div>
-                        </div>
-                    <div class="column-right">                              
-                        <div class="services-video3"> 
-                            <video width="500" height="375" autoplay loop>
-                                <source src="../eecs-library/img/services-video3.mp4" type="video/mp4">
-                                <source src="../eecs-library/img/services-video3.mp4" type="video/ogg">
-                                Your browser does not support the video tag.
-                              </video>
-                        </div>
                     </div>
                 </div>
-
-                <div class="row">
-                    <div class="column-left">
-                        <div class="book-club">
-                            <div class="col-xl-11">
-                            <h4>Book club</h4>
-                            <p>Life is short, and books take a long time to read, so let's make our reading count! 
-                                Variuos book clubs are available for those who enjoy reading all kinds of genres.</p>
-                            <p>All students are welcome whether you are a veteran or are reading a great work for your first time. 
-                                We will discuss these books to enlarge our understanding of the ideas that shape the human condition, 
-                                but our discussions will be casual and we won't take ourselves too seriously.</p>
-                            </div>
-                        </div>
-                    </div>
-                <div class="column-right">                              
-                    <div class="services-video4"> 
-                        <video width="500" height="375" autoplay loop>
-                            <source src="../eecs-library/img/services-video4.mp4" type="video/mp4">
-                            <source src="../eecs-library/img/services-video4.mp4" type="video/ogg">
-                            Your browser does not support the video tag.
-                          </video>
-                    </div>
-                </div>
-            </div>
-
-                    </div>
-                </div>
-            </section>
-            <section class="padding-for-bottom"></section>
+        </section>
+        
+        <section class="padding-for-bottom"></section>
 
     </main>
     
