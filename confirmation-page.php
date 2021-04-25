2439fc62c24c7fee8a714c30ba2084c0fc36e1d0
xshah01 <shadaba@kth.se>
2021-04-25 21:58:58 +0200

    countdown

--- confirmation-page.php
+++ confirmation-page.php
@@ -27,6 +27,24 @@
 
                     <h4>Please pick up your book within 24h</h4>
                     <p>or your reservation will be terminated</p>
+
+                    <br><br>
+
+                    <div class="countdown text-center">
+                        <div id="clockdiv"><div>
+                            <span class="hours"></span>
+                        </div>
+                        <div>
+                            <span class="minutes"></span>
+                        </div>
+                        <div>
+                            <span class="seconds"></span>
+                        </div>
+                        </div>
+                    </div>
+
+                    <br><br><br>
+
                     <p>Click <a href="contact.php">here </a> to find your library</p>
 
                     <div class="clearfix"></div>
@@ -39,6 +57,46 @@
         </section>
 
     <!-- ==================== End Book Area ==================== -->
+
+    <script type="text/javascript">
+        function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+        }
+
+        function initializeClock(id, endtime) {
+        var clock = document.getElementById(id);
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
+
+        function updateClock() {
+            var t = getTimeRemaining(endtime);
+
+            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+            if (t.total <= 0) {
+            clearInterval(timeinterval);
+            }
+        }
+
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+        }
+
+        var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+        initializeClock('clockdiv', deadline);
+    </script>
 
     </main>
     
