e749b1d2c61710d81bc07d1dc2eac1de4d797023
xshah01 <shadaba@kth.se>
2021-04-25 06:59:14 +0200

    included php in admin dashboard

--- admin.php
+++ admin.php
@@ -96,21 +96,62 @@
                     </div>            
                     <div class="col-3">
                         <div class="box text-center ">
-                            <h1>5</h1>
+
+                            <?php 
+                            
+                            include('config.php');
+
+                                $sql = "SELECT * FROM tbl_category";    //SQL query
+
+                                $res = mysqli_query($conn, $sql);   //Execute query
+
+                                $count = mysqli_num_rows($res); //Count rows
+
+                            ?>
+
+                            <h1><?php echo $count ?></h1>
                             <br />
                             Categories
                         </div>
                         <div class="box text-center ">
-                            <h1>5</h1>
+
+                            <?php 
+                            
+                            include('config.php');
+
+                                $sql2 = "SELECT * FROM tbl_book";    //SQL query
+
+                                $res2 = mysqli_query($conn, $sql2);   //Execute query
+
+                                $count2 = mysqli_num_rows($res2); //Count rows
+
+                            ?>
+
+                            <h1><?php echo $count2 ?></h1>
                             <br />
-                            Categories
+                            Books
                         </div>
                         <div class="box text-center ">
-                            <h1>5</h1>
+
+                            <?php 
+                            
+                            include('config.php');
+
+                                $sql3 = "SELECT * FROM tbl_reservation WHERE status='Reserved' ";    //SQL query
+
+                                $res3 = mysqli_query($conn, $sql3);   //Execute query
+
+                                $count3 = mysqli_num_rows($res3); //Count rows
+
+                            ?>
+                            <h1><?php echo $count3 ?></h1>
                             <br />
-                            Categories
+                            Reservations
                         </div>
-                    </div>    
+                    </div>
+                    
+                    <br><br><br>
+                    
                     <div class="clearfix"></div>
                 </div>
             </div>
