9fb0945b55e6272b0a3ab9fe93cdeeb1d0c964bb
xshah01 <shadaba@kth.se>
2021-04-30 01:17:03 +0200

    new siteurl

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
     session_start();
 
     //Create constants to store non repeating values
-    define('SITEURL', 'http://localhost:8080/eecs-library/');
+    define('SITEURL', 'http://localhost/eecs-library/');
 
     //Connect to database
     $conn = mysqli_connect('localhost', 'root', '') or die(mysqli_error());
