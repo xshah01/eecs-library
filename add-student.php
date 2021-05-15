37f732571be7b68576fc47fa0c5c0d8fbaf0e447
xshah01 <shadaba@kth.se>
2021-05-15 22:55:57 +0200

    checkbox

--- add-student.php
+++ add-student.php
@@ -162,12 +162,12 @@ include('config.php');
         <input id="password" type="password" name="password" required/>
         <label for="confirm_password">Re-enter password</label>
         <input id="confirm_password" type="password" name="confirm_password" required/>
-        <div class="checkbox">
-            <input id="checkbox" name="condition" type="checkbox" required>
-        </div>
-        <label for="condition" class="checkbox-text">By checking this box, you indicate that you have read and understood our
-            <a href="book-condition-guidelines.php">user terms</a>, our <a href="">personal data management</a> and our <a href="">cookie policy.</a>
-        </label>
+            <div class="checkbox">
+                <input id="checkbox" name="condition" type="checkbox" required>
+            </div>
+            <label for="condition" class="checkbox-text">By checking this box, you indicate that you have read and understood our
+                <a href="" onclick="userTerms()">user terms</a>, our <a href="" onclick="GDPR()">personal data management</a> and our <a href="" onclick="cookies()">cookie policy.</a>
+            </label>
         <input type="submit" class="btn-sign-in" name="submit" value="Sign up" />
         </form>
         <footer>
@@ -179,6 +179,24 @@ include('config.php');
 </div> 
 </body>
 </html>
+
+<script type="text/javascript">
+    function userTerms() {
+        alert("To use our service, you must be 18 years old or have a parent's approval. We are happy to see that only students use the service, but this is not mandatory. The service is not provided to persons who have previously violated our User Terms. It is up to you as a user to collect your reserved book within 24 hours, and avoid the risk of getting your reservation terminated. It is your responsibility to return borrowed book(s) and in perfect condition once the loan period of 60 days (including holidays) is due time. You have 2 days to return the book(s). If this return period is overdrawn, a late fee will be issued.");
+    }
+</script>
+
+<script type="text/javascript">
+    function GDPR() {
+        alert("At EECS-LIBRARY, we care about your privacy on the web and therefore try to collect as little sensitive personal information from you as possible - just enough for the website to work for you and all other users. We store your e-mail address so that you can log in to the site, so that we can notify you of changes in User Terms, Policies or Personal Data Processing. We save your phone number so that we can contact you. However, it is optional if the phone number or e-mail address is to be used as the contact method. We save your name so that we know who you are when you reserves/loan book(s). All personal information we store about you is the information you enter in the registration form or update via your personal page. If you do not enter information such as name and e-mail address, it will not be possible to create an account as this information is necessary for our member functions.");
+    }
+</script>
+
+<script type="text/javascript">
+    function cookies() {
+        alert("As in common practice with almost all professional websites, this website uses cookies, which are small files that are downloaded to your computer to improve your experience. We use Account Related Cookies for handling the registration process and general administration. These cookies are usually deleted when you log out, but in some cases they may remain afterwards to remember your website settings when you log out. Form Related Cookies are also utilised when you send data via a form such as the one on the login page. These cookies can be set to remember your user information for future correspondence. To preserve the user integrity, Third Party Cookies are not in usage.");
+    }
+</script>
 
 <script type="text/javascript">
 $(document).ready(function () {
