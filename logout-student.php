1b46c1782b588e9f800624c6d4a78839e24710af
xshah01 <shadaba@kth.se>
2021-07-12 20:44:56 +0200

    new redirection after logut

--- logout-student.php
+++ logout-student.php
@@ -4,6 +4,6 @@
 
     session_destroy();  //Unsets $_SESSION['$email']
 
-    header("location: ".SITEURL.'login-student.php'); //Redirect to login page
+    header("location: ".SITEURL.'home.php'); //Redirect to home page
 
 ?>
